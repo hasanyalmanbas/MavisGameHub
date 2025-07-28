fde52609094fff8c059da8c57aee6f6b47508732
Hasan Talha Yalmanbas <hasanyalmanbas@gmail.com>
2025-07-28 14:50:07 +0300

    Create Hub layer and scene

---
+++ Assets/_Project/Hub/Installers/HubInstaller.cs
@@ -0,0 +1,12 @@
+using Reflex.Core;
+using UnityEngine;
+
+namespace _Project.Hub.Installers
+{
+    public class HubInstaller : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder builder)
+        {
+        }
+    }
+}
