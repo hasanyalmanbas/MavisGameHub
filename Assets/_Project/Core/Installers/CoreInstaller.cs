09191ad87f8fe03be55eca19b24d580c7e55ed1a
Hasan Talha Yalmanbas <hasanyalmanbas@gmail.com>
2025-07-28 14:22:32 +0300

    Create Core layer and installer

---
+++ Assets/_Project/Core/Installers/CoreInstaller.cs
@@ -0,0 +1,12 @@
+using Reflex.Core;
+using UnityEngine;
+
+namespace _Project.Core.Installers
+{
+    public class CoreInstaller : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder builder)
+        {
+        }
+    }
+}
